01be2d48e52484fa88d62f4752234d674af33d24
FabricioCrv <fabriciogarciacarvalho87@hotmail.com>
2026-04-29 11:17:23 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,101 @@
-import styled from 'styled-components';
-import LogoBranca from '../LogoBranca';
+import styled from "styled-components";
+import LogoBranca from "../LogoBranca";
+import { FaPhoneAlt, FaWhatsapp, FaEnvelope, FaMapMarkerAlt, FaInstagram, FaYoutube, FaFacebook } from "react-icons/fa";
 
 const FooterContainer = styled.footer`
-    background-color: #1f4a8a;
-    display: flex;
-    align-items: center;
-    padding: 0 20px;
-    height: 80px; /* Altura fixa para o header */
-    box-shadow: 0 2px 4px rgba(0,0,0,0.1); /* Sombra suave como na imagem */
-`
+  background-color: #1f4a8a;
+  color: #fff;
+  padding: 40px 60px 20px;
+`;
 
-function Footer(){
-    return(
-        <FooterContainer>
-           <LogoBranca/>
-        </FooterContainer>
-    )
+const FooterContent = styled.div`
+  display: grid;
+  grid-template-columns: 1.4fr 1fr 1.5fr 1fr;
+  align-items: start;
+  gap: 60px;
+`;
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 180px;
+`;
+
+export const Title = styled.h4`
+  margin-bottom: 15px;
+  font-size: 18px;
+`;
+
+const Text = styled.p`
+  display: flex;
+  align-items: flex-start;
+  gap: 10px;
+  margin: 6px 0;
+  font-size: 14px;
+
+  svg {
+    min-width: 16px;
+    margin-top: 3px;
+  }
+`;
+
+const BottomBar = styled.div`
+  border-top: 1px solid rgba(255, 255, 255, 0.3);
+  margin-top: 30px;
+  padding-top: 15px;
+
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  font-size: 14px;
+`;
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <FooterContent>
+        <Column>
+          <LogoBranca />
+        </Column>
+
+        <Column>
+          <Title>Menu</Title>
+          <Text>Início</Text>
+          <Text>Sobre</Text>
+          <Text>Blog</Text>
+          <Text>Contato</Text>
+        </Column>
+
+        <Column>
+            <Title>Contatos</Title>
+            <Text><FaPhoneAlt/> 85 99184-5926</Text>
+            <Text><FaWhatsapp/> 85 99184-5926</Text>
+            <Text><FaEnvelope/> contato@fjraudiovisual.com.br</Text>
+            <Text>
+                <FaMapMarkerAlt />
+                <span>
+                Avenida Coronel Miguel Dias, 50
+                Sl 701, Fortaleza, Brazil
+                </span>
+            </Text>
+        </Column>
+
+        <Column>
+            <Title>Redes Sociais</Title>
+            <div style={{ display: "flex", gap: "15px", marginTop: "10px"}}>
+                <FaInstagram/>
+                <FaYoutube/>
+                <FaFacebook/>
+            </div>
+        </Column>
+      </FooterContent>
+
+      <BottomBar>
+        <span>
+          FJR - Produções Audiovisuais © 2026 Todos os direitos reservados.
+        </span>
+      </BottomBar>
+    </FooterContainer>
+  );
 }
 
 export default Footer;
--- src/components/LogoBranca/index.js
+++ src/components/LogoBranca/index.js
@@ -4,7 +4,6 @@ import logoBranca from '../../images/logo-branca-fjr.png';
 const LogoContainer = styled.div`
   display: flex;
   align-items: center;
-  margin-right: 500px;
 `
 const LogoImage = styled.img`
     width: 240px;
